e3003b3e496e646ced7bd289c9fd9daedb09bee4
Sreevishnu Damodaran <75064942+sreevishnu-damodaran@users.noreply.github.com>
2022-05-07 01:04:59 +0530

    Fixed sentiment weights url

--- app/sentiment_model.py
+++ app/sentiment_model.py
@@ -1,7 +1,7 @@
 import os
 import urllib
 
-import joblib
+import pickle
 from tqdm import tqdm
 
 
@@ -20,20 +20,20 @@ def download_weights(url, out_path):
 
 
 def sentiment_model(sample):
-    weights_file = "movie_sentiment.joblib"
-    url = "https://api.wandb.ai/files/akshaydevwandb/uncategorized/"\
-        "2hldjwkn/movie_sentiment.joblib"
+    weights_file = "movie_sentiment.pkl"
+    url = "https://github.com/akshaydevml/lazy-film-reviews/releases/download"\
+    "/v0.0/movie_sentiment.pkl"
 
     if not os.path.exists(weights_file):
         download_weights(url, ".")
 
     try:
-        sentiment_analysis = joblib.load(open(weights_file, 'rb'))
+        sentiment_analysis = pickle.load(open(weights_file, 'rb'))
     except Exception as e:
         print("Couldn't read model weights:", e)
         print("Downloading weights again.")
         download_weights(url, ".")
-        sentiment_analysis = joblib.load(open(weights_file, 'rb'))
+        sentiment_analysis = pickle.load(open(weights_file, 'rb'))
 
     prediction = sentiment_analysis.predict([sample])
     prediction = str(prediction[0])
